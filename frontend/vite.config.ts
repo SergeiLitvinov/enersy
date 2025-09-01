4683442a01f3cc04f7aa0e4b68af0f83fa691102
SergeyLitvinov <litvinov.sn@yandex.ru>
2025-09-01 08:48:30 +0300

    vite config update for render.com

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['enersy.onrender.com', 'localhost', '127.0.0.1'],
     port: 3000,
     host: '0.0.0.0',
     hmr: {
